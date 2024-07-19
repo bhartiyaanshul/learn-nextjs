b0101946a25f8f4f5db310ff856565b238cddad2
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-07-19 10:42:04 +0530

    Page Not Found added

---
+++ first-next-project/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+            <h3>Sorry, the page you are looking for does not exist.</h3>
+        </div>
+    )
+}

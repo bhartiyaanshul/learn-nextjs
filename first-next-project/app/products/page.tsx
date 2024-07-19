1a815469b03789460dd203f7019b4ecfccfc2965
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-07-19 13:40:01 +0530

    Link Component Navigation Added

--- first-next-project/app/products/page.tsx
+++ first-next-project/app/products/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link"
+
 export default function Products() {
     return (
         <div>
             <h1>Products</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <h3><Link href='products/1'>Product 1</Link></h3>
+            <h3><Link href='products/2'>Product 2</Link></h3>
+            <h3><Link href='products/3'>Product 3</Link></h3>
+            <h3><Link href='products/4'>Product 4</Link></h3>
+            <h3><Link href='products/5'>Product 5</Link></h3>
         </div>
     )
 }

b3db39aa9de00817ab1b3e7e7a434349edd90894
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-07-20 18:35:57 +0530

    Error handling done

--- first-next-project/app/products/[productId]/page.tsx
+++ first-next-project/app/products/[productId]/page.tsx
@@ -1,5 +1,11 @@
+ 
+
 import { notFound } from "next/navigation"
 import { Metadata } from "next"
+
+function getRandomNumber(count: number) {
+    return Math.floor(Math.random() * count);
+}
 
 type Props = {
     params: {
@@ -11,12 +17,14 @@ export const generateMetadata = ({ params }: Props): Metadata => {
     return {
         title: `Product ${params.productId} Details`
     }
-
 }
 
 
 export default function ProductDetails({ params }: Props) {
-
+    const random = getRandomNumber(2)
+    if (random === 1) {
+        return <div>Random Error</div>
+    }
     if (params.productId > 100) {
         return notFound()
     }
